e952f4e333194e3b071eec2ad513d692b36eb5ab
Iman Manati <40170427+imanbarati@users.noreply.github.com>
2025-05-24 14:45:41 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://selemene.ir"
+          href="https://crmmanan.ir"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -57,7 +57,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://selemene.ir"
+          href="https://tavpos.com"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
