8336fa08d306151f546c5fdc3726a9873b46f06e
Iman Manati <40170427+imanbarati@users.noreply.github.com>
2025-05-25 08:25:51 +0330

    Create page2.tsx

---
+++ src/app/page2.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          شروع به کار کنید&nbsp;
+          <code className="font-mono font-bold"></code>
+        </p>
+      </div>
+<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSCuVNREuf_wHG08vu2VCNCWw9A68T6B7v1VBlLQwbMfdpy71QGA1t7LQqALGCM8QVWs065Z8B5oOCl/pubhtml?widget=true&amp;headers=false"></iframe>
